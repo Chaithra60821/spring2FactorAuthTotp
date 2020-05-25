e0d1a083f1e15638321f67ab51173a804710e3ae
chaithra60821 <Chaithra60821@gmail.com>
2020-05-26 00:48:44 +0530

    Adding data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO USER1(id,active,password,roles,username, secret)
+VALUES(1,true,'user','ROLE_USER','user', 'JBSWY3DPEHPK3PXP')
+
+INSERT INTO USER1(id,active,password,roles,username, secret)
+VALUES(2,true,'admin','ROLE_ADMIN','admin', 'JBSWY3DPEHPK3PXP')
