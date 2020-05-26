bc38f2421ca1fc05ba7208d574c38472cf08db3f
Chaithra <chaithraks093@gmail.com>
2020-05-26 21:55:11 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
+
+-- failing to load this data. I havenpt solve that for now just inserted these records in the h2DB
+-- http://localhost:8080/h2-console [db details are present in the application.properties file]
+
 INSERT INTO USER1(id,active,password,roles,username, secret)
 VALUES(1,true,'user','ROLE_USER','user', 'JBSWY3DPEHPK3PXP')
 
